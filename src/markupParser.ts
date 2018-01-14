99bb94abe55c856de40c211e3a31ec199203b24e
Denis Baumgärtner <denco@freenet.de>
2018-01-14 17:37:00 +0100

    add rendering of code blocks and text styles

--- src/markupParser.ts
+++ src/markupParser.ts
@@ -15,49 +15,92 @@ function emoticonUri(emoticonFile: string) {
 }
 
 export async function parseMarkup(sourceText: string, sourceUri: vscode.Uri) {
+	//TODO: use Tokenazer instead of line loop
+
 	var result = '';
 
-	let openTag = '';
+	let listTag = '';
+	let codeTagFlag = 0;
 	for (let entry of sourceText.split(/\n/gi)) {
-		let tag = entry.replace(/h(\d+)\.\s([^\n]+)/g, "<h$1>$2</h$1>");
-		tag = tag.replace(/-{4,}/gi, '<hr />');
-		tag = tag.replace(/-{3}/gi, '&mdash;');
-		tag = tag.replace(/-{2}/gi, '&ndash;');
-		tag = tag.replace(/:\)/g, '<img alt="(smile)" src="' + emoticonUri('smile.png') + '"/>');
-		tag = tag.replace(/:\(/g, '<img alt="(sad)" src="' + emoticonUri('sad.png') + '"/>');
-		tag = tag.replace(/:P/g, '<img alt="(cheeky)" src="' + emoticonUri('tongue.png') + '"/>');
-		tag = tag.replace(/:D/g, '<img alt="(laugh)" src="' + emoticonUri('biggrin.png') + '"/>');
-		tag = tag.replace(/;\)/g, '<img alt="(wink)" src="' + emoticonUri('wink.png') + '"/>');
-		tag = tag.replace(/\(y\)/g, '<img alt="(thumbs-up)" src="' + emoticonUri('thumbs_up.png') + '"/>');
-		tag = tag.replace(/\(n\)/g, '<img alt="(thumbs-down)" src="' + emoticonUri('thumbs_down.png') + '"/>');
-		tag = tag.replace(/\(i\)/g, '<img alt="(information)" src="' + emoticonUri('information.png') + '"/>');
-		tag = tag.replace(/\(\/\)/g, '<img alt="(tick)" src="' + emoticonUri('check.png') + '"/>');
-		tag = tag.replace(/\(x\)/g, '<img alt="(cross)" src="' + emoticonUri('error.png') + '"/>');
-		tag = tag.replace(/\(!\)/g, '<img alt="(warning)" src="' + emoticonUri('warning.png') + '"/>');
+		let tag = entry;
 
-		tag = tag.replace(/\[([^|]*)?\|?([^|]*)\]/, "<a href='$2'>$1</a>");
+		if (codeTagFlag == 0) {
+			tag = tag.replace(/h(\d+)\.\s([^\n]+)/g, "<h$1>$2</h$1>");
+			tag = tag.replace(/-{4,}/gi, '<hr />');
+			tag = tag.replace(/-{3}/gi, '&mdash;');
+			tag = tag.replace(/-{2}/gi, '&ndash;');
 
-		let re = /^([-|\*|#]+)\s(.*)/;
+			tag = tag.replace(/_([^_]*)_/g, "<em>$1</em>");
+			tag = tag.replace(/\+([^\+]*)\+/g, "<u>$1</u>");
+			tag = tag.replace(/\^([^\^]*)\^/g, "<sup>$1</sup>");
+			tag = tag.replace(/~([^~]*)~/g, "<sub>$1</sub>");
+			tag = tag.replace(/\{{2}(.*)\}{2}/g, "<code>$1</code>");
+			tag = tag.replace(/\?{2}(.*)\?{2}/g, "<cite>$1</cite>");
+			tag = tag.replace(/\{color:(\w+)\}(.*)\{color\}/g, "<span style='color:$1;'>$2</span>");
+
+			tag = tag.replace(/bq. (.*)/g, "<blockquote><p>$1</p></blockquote>");
+			tag = tag.replace(/\{quote\}(.*)\{quote\}/g, "<blockquote><p>$1</p></blockquote>");
+
+
+			tag = tag.replace(/:\)/g, '<img alt="(smile)" src="' + emoticonUri('smile.png') + '"/>');
+			tag = tag.replace(/:\(/g, '<img alt="(sad)" src="' + emoticonUri('sad.png') + '"/>');
+			tag = tag.replace(/:P/g, '<img alt="(cheeky)" src="' + emoticonUri('tongue.png') + '"/>');
+			tag = tag.replace(/:D/g, '<img alt="(laugh)" src="' + emoticonUri('biggrin.png') + '"/>');
+			tag = tag.replace(/;\)/g, '<img alt="(wink)" src="' + emoticonUri('wink.png') + '"/>');
+			tag = tag.replace(/\(y\)/g, '<img alt="(thumbs-up)" src="' + emoticonUri('thumbs_up.png') + '"/>');
+			tag = tag.replace(/\(n\)/g, '<img alt="(thumbs-down)" src="' + emoticonUri('thumbs_down.png') + '"/>');
+			tag = tag.replace(/\(i\)/g, '<img alt="(information)" src="' + emoticonUri('information.png') + '"/>');
+			tag = tag.replace(/\(\/\)/g, '<img alt="(tick)" src="' + emoticonUri('check.png') + '"/>');
+			tag = tag.replace(/\(x\)/g, '<img alt="(cross)" src="' + emoticonUri('error.png') + '"/>');
+			tag = tag.replace(/\(!\)/g, '<img alt="(warning)" src="' + emoticonUri('warning.png') + '"/>');
+
+			tag = tag.replace(/\[([^|]*)?\|?([^|]*)\]/g, "<a href='$2'>$1</a>");
+		}
+
+		// code
+		// online code tag
+		tag = tag.replace(/\{code[^\}]*\}(.*)\{code\}/, "<pre><code>$1</code></pre>");
+		let re = /\{code.*\}/;
 		let match = tag.match(re);
 		if (match) {
-			if (openTag.length == 0) {
-				if (match[1] == '#') {
-					openTag = 'ol';
-				} else {
-					openTag = 'ul'
-				}
-				tag = '<' + openTag + '>';
+			if (codeTagFlag == 0) {
+			tag = '<pre><code>';
+			codeTagFlag = 1;
 			} else {
-				tag = '';
+				tag = '</pre></code>';
+				codeTagFlag = 0;
 			}
-			tag += "<li>" + match[2] + "</li>";
 		}
 
-		if ((tag.length == 0) && (openTag.length != 0)) {
-			tag += '</' + openTag + '>';
-			openTag = '';
-		}
+		if (codeTagFlag == 0) {
+			// lists
+			re = /^([-|\*|#]+)\s(.*)/;
+			match = tag.match(re);
+			if (match) {
+				if (listTag.length == 0) {
+					if (match[1] == '#') {
+						listTag = 'ol';
+					} else {
+						listTag = 'ul';
+						if (match[1] == '-'){
+							listTag += ' style="list-style-type: square;"'
+						}
+					}
+					tag = '<' + listTag + '>';
+				} else {
+					tag = '';
+				}
+				tag += "<li>" + match[2] + "</li>";
+			}
 
+			if ((tag.length == 0) && (listTag.length != 0)) {
+				tag += '</' + listTag + '>';
+				listTag = '';
+			}
+
+			tag = tag.replace(/\*([^\*]*)\*/g, "<strong>$1</strong>");
+			// tag = tag.replace(/-(.*)-/g, "<span style='text-decoration: line-through;'>striket-hrough</span>");
+	}
 		console.log("PARSED:" + tag);
 		result += tag + '<br />';
 	}
