db82d88ef14197b26199518f7db17f6d1c555ade
Denis Baumgärtner <denis.baumgaertner@btelligent.com>
2021-03-25 19:38:21 +0100

    fix code/noformat rendering

--- src/markupParser.ts
+++ src/markupParser.ts
@@ -143,7 +143,7 @@ export function parseMarkup(sourceUri: vscode.Uri, sourceText: string) {
 			return `<pre><code style='font-family: ${MONOSPACE_FONT_FAMILY}'>${m2.replace(/</gi, '&lt;')}</code></pre>`;
 		});
 
-		const code_re = /\{[(code)|(noformat)].*\}/;
+		const code_re = /\{(noformat)|(code)[^}]*\}/;
 		const code_match = tag.match(code_re);
 		if (code_match) {
 			if (! codeTagFlag) {
@@ -156,8 +156,8 @@ export function parseMarkup(sourceUri: vscode.Uri, sourceText: string) {
 		}
 
 		const panel_re = /\{panel(.*)}/;
-		if (tag.match(panel_re)) {
-			if (! panelTagFlag) {
+		if (! codeTagFlag && tag.match(panel_re)) {
+			if (! panelTagFlag ) {
 				let panelStyle = "";
 				let titleStyle = "";
 				tag = tag.replace(panel_re, function (m0, m1) {
