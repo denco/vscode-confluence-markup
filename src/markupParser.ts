b3fe410fa69d3dedd52cfbf64e9d830a5c5ffec9
Nate Henry <NSHenry@users.noreply.github.com>
2023-10-06 10:26:15 -0400

    Fixed simple code block issue.

--- src/markupParser.ts
+++ src/markupParser.ts
@@ -54,7 +54,7 @@ export function parseMarkup(sourceUri: vscode.Uri, sourceText: string) {
 	let listStyle = '';
 	let codeTagFlag = false;
 	let innerCodeTagFlag = false;
-	let codeBlockTagFlag = false;
+	let paneledCodeTagFlag = false;
 	let panelTagFlag = false;
 	let tableFlag = false;
 	let listFlag = false;
@@ -161,19 +161,21 @@ export function parseMarkup(sourceUri: vscode.Uri, sourceText: string) {
 		}
 
 		// code
-		// online code tag
+		// one line code tag
 		tag = tag.replace(/\{(noformat|code)[^}]*\}(.*)\{(noformat|code)\}/, function (m0, m1, m2) {
 			return `<pre><code style='font-family: ${MONOSPACE_FONT_FAMILY}'>${m2.replace(/</gi, '&lt;')}</code></pre>`;
 		});
 
 
 		// code panel tag
+		const code_re = /\{code[^}]*\}/;
+		const code_match = tag.match(code_re);
 		const code_panel_open_re = /\{code:(.*)\}/;
 		const code_panel_open_match = tag.match(code_panel_open_re);
 		const code_panel_close_re = /\{code\}/;
 		const code_panel_close_match = tag.match(code_panel_close_re);
-		if (code_panel_open_match) {
-			if (! codeBlockTagFlag) {
+		if (code_match) {
+			if (code_panel_open_match && ! paneledCodeTagFlag) {
 				let codeBlockStyle = "";
 				// let titleStyle = "";
 				tag = tag.replace(code_panel_open_re, function (m0, m1) {
@@ -218,36 +220,44 @@ export function parseMarkup(sourceUri: vscode.Uri, sourceText: string) {
 					// res = res.replace('$titleStyle', titleStyle);
 					return res;
 				});
-				codeBlockTagFlag = true;
+				paneledCodeTagFlag = true;
+				// If the opening match isn't found. Check for a simple code block by looking for the closing tag without an innner code tag.
 			}
-			} else if (codeBlockTagFlag && ! code_panel_open_match && ! code_panel_close_match) {
-				// Flag the inner code, so it doesn't get modified.
-				innerCodeTagFlag = true;
-			} else if (code_panel_close_match) {
-				tag = '</code></pre></div>';
-				//This pays attention to the list flag and adds the closing </li> tag if needed.
-				if (listFlag) {
-					tag = `${tag}</li>`;
-				}
-				innerCodeTagFlag = false;
-				codeBlockTagFlag = false;
+			if (! innerCodeTagFlag && code_panel_close_match && ! paneledCodeTagFlag) {
+				tag = '<div class="code-panel"><pre><code>';
+				paneledCodeTagFlag = true;
+			}
+		}
+		// if (paneledCodeTagFlag && ! code_panel_open_match && ! code_panel_close_match) {
+		if (paneledCodeTagFlag && ! code_match) {
+			// Flag the inner code, so it doesn't get modified.
+			innerCodeTagFlag = true;
+		}
+		if (code_panel_close_match && paneledCodeTagFlag && innerCodeTagFlag) {
+			tag = '</code></pre></div>';
+			//This pays attention to the list flag and adds the closing </li> tag if needed.
+			if (listFlag) {
+				tag = `${tag}</li>`;
+			}
+			innerCodeTagFlag = false;
+			paneledCodeTagFlag = false;
 		}
 
-		// old code tag
-		const code_re = /\{code[^}]*\}/;
-		const code_match = tag.match(code_re);
-		if (code_match) {
-			if (! codeTagFlag) {
-				tag = `<pre><code style='font-family: ${MONOSPACE_FONT_FAMILY}'>`;
-				codeTagFlag = true;
-			} else {
-				tag = '</pre></code>';
-				codeTagFlag = false;
-			}
-		}
+		// original code block. keeping for reference.
+		// const code_re = /\{code[^}]*\}/;
+		// const code_match = tag.match(code_re);
+		// if (code_match) {
+		// 	if (! codeTagFlag) {
+		// 		tag = `<pre><code style='font-family: ${MONOSPACE_FONT_FAMILY}'>`;
+		// 		codeTagFlag = true;
+		// 	} else {
+		// 		tag = '</pre></code>';
+		// 		codeTagFlag = false;
+		// 	}
+		// }
 
 		const panel_re = /\{panel(.*)}/;
-		if (! codeBlockTagFlag && tag.match(panel_re)) {
+		if (! paneledCodeTagFlag && tag.match(panel_re)) {
 			if (! panelTagFlag ) {
 				let panelStyle = "";
 				let titleStyle = "";
@@ -361,7 +371,7 @@ export function parseMarkup(sourceUri: vscode.Uri, sourceText: string) {
 					listArr = listArr.slice(0, li_match[1].length);
 				}
 				// This prevents the closing </li> tag from being added too prematurely.
-				if (codeBlockTagFlag || panelTagFlag) {
+				if (paneledCodeTagFlag || panelTagFlag) {
 					tag += "<li>" + li_match[2];
 				} else {
 					tag += "<li>" + li_match[2] + "</li>";
