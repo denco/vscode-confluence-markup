a0ba9f553aed5f4cecc024ab4fafb63979b6e91d
Denis Baumgärtner <denco@freenet.de>
2023-11-03 21:02:39 +0100

    fix some lint issues

--- src/test/suite/index.ts
+++ src/test/suite/index.ts
@@ -1,9 +1,8 @@
 import * as path from 'path';
 import * as Mocha from 'mocha';
+import { globSync } from 'glob';
 
 export function run(): Promise<void> {
-
-	const { globSync } = require("glob");
 
 	// Create the mocha test
 	const mocha = new Mocha({
